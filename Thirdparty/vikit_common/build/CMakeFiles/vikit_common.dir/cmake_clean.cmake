87143b804b0eef96ac840479b86fa5128cb248ea
LiYang <yli@miivii.com>
2016-12-08 09:49:26 +0800

    add some files

---
+++ Thirdparty/vikit_common/build/CMakeFiles/vikit_common.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/vikit_common.dir/src/math_utils.cpp.o"
+  "CMakeFiles/vikit_common.dir/src/vision.cpp.o"
+  "CMakeFiles/vikit_common.dir/src/performance_monitor.cpp.o"
+  "CMakeFiles/vikit_common.dir/src/pinhole_camera.cpp.o"
+  "../lib/libvikit_common.pdb"
+  "../lib/libvikit_common.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/vikit_common.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Thirdparty/vikit_common/build/vikit_commonConfig.cmake
@@ -0,0 +1,17 @@
+#######################################################
+# vikit_common source dir
+set( vikit_common_SOURCE_DIR "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common")
+
+#######################################################
+# vikit_common build dir
+set( vikit_common_DIR "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common/build")
+
+#######################################################
+set( vikit_common_INCLUDE_DIR  "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common/include" )
+set( vikit_common_INCLUDE_DIRS "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common/include" )
+
+set( vikit_common_LIBRARIES    "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common/lib/libvikit_common.so" )
+set( vikit_common_LIBRARY      "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common/lib/libvikit_common.so" )
+
+set( vikit_common_LIBRARY_DIR  "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common/build" )
+set( vikit_common_LIBRARY_DIRS "/home/ubuntu/catkin_ws/src/orbslam_dwo/Thirdparty/vikit_common/build" )
